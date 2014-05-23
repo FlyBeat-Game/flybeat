b0d1b04aa5db9573d04f440e91a37c2374dcc02c
AndreBaptista <amccb@student.dei.uc.pt>
2014-05-23 17:19:01 +0100

    Melody detection improved.

--- matlab2/getMaxs.m
+++ matlab2/getMaxs.m
@@ -1,13 +1,15 @@
-function maxs = getMaxs(x,n)
+function [maxs, peaks] = getMaxs(x,n)
    if (n > length(x))
        n = length(x);
    end
    
    maxs = zeros(1,n);
+   peaks = zeros(1,n);
    for i=1:n
        m = getMax(x);
-       maxs(i) = m; %x(m);
+       maxs(i) = m;
        if (m ~= 0)
+        peaks(i) = x(m);
         x(m) = 0;
        end
    end
