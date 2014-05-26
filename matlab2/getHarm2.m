b0a0b620ac8f6a15258fced028be47a2d9ade644
AndreBaptista <amccb@student.dei.uc.pt>
2014-05-26 15:12:26 +0100

    Melody detection.

--- matlab2/getHarm2.m
+++ matlab2/getHarm2.m
@@ -1,20 +1,31 @@
-function m = getHarm(f)
-    %f = f(1:floor(length(f)/2));
+function m = getHarm2(f)
+    f = f(1:floor(length(f)/2));
     
-    %[f,mag] = getMaxs(f,20);
+    [f,mag] = getMaxs(f,20);
 
     harmonics = zeros(1,length(f));
     gcds = zeros(length(f),length(f));
-    x = 1;
+    g = zeros(1,20);
     
     for i=1:length(f)
         for j=1:length(f)
             if (i ~= j)
-                g = gcd(f(i),f(j));
-                gcds(i,j) = g;
+                gcds(i,j) = gcd(f(i),f(j));
             end
         end
     end
-    gcds
-    mode(mode(gcds))
+    m = 0;
+    gcds = gcds./(gcds~=0);
+    gcds = gcds./(gcds~=1);
+    m = mode(gcds(:));
+    if (m == Inf)
+        m = 0;
+    end
 end
+
+%40.44
+%22.9
+%8.7
+%6.91
+%5.67
+
